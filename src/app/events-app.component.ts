e1c667adbfbbb90b0a764737b387ef1a954f63ee
collinewait <collinewait17@gmail.com>
2019-04-20 15:30:30 +0300

    change prefix from app to events

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-events',
-  template: '<app-events-list><app-events-list>'
+  selector: 'events-app',
+  template: '<events-list><events-list>'
 })
 export class EventsAppComponent {
   title = 'app';
