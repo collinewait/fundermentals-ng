d5837ed0385ce6fb9613d929584a7462b1f75115
collinewait <collinewait17@gmail.com>
2019-04-21 22:15:09 +0300

    create a template based form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,19 @@
+import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { userRoutes } from './user.routes';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   providers: [
   ]
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,5 +1,8 @@
+import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
+import { Routes } from '@angular/router';
 
-export const userRoutes = [
-  { path: 'profile', component: ProfileComponent }
+export const userRoutes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent}
 ];
