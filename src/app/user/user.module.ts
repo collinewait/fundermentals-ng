5c71dce1d3b3a6e75358a400f1b7c4f75ed8d7bd
collinewait <collinewait17@gmail.com>
2019-04-22 11:43:13 +0300

    create a reactive form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,13 +2,14 @@ import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { userRoutes } from './user.routes';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
