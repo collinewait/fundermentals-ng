d051c16ef9f049d22e90c53c4463acfa22d9b25b
collinewait <collinewait17@gmail.com>
2019-04-23 20:08:41 +0300

    use @ViewChild to get access to the local ref variable and close the modal

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { JQ_TOKEN } from './jquery.service';
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
 @Component({
   selector: 'simple-modal',
   template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalContainer class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -11,7 +12,7 @@ import { Component, Input } from '@angular/core';
             </button>
             <h4 class="modal-title">{{title}}</h4>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" (click)="closeModal()">
             <ng-content></ng-content>
           </div>
         </div>
@@ -25,4 +26,11 @@ import { Component, Input } from '@angular/core';
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalContainer') containerEl: ElementRef;
+
+  constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+  closeModal() {
+    this.$(this.containerEl.nativeElement).modal('hide');
+  }
 }
