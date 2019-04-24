b09657db733c9e6291ab7b65553cbac5ea7486e3
collinewait <collinewait17@gmail.com>
2019-04-24 11:28:36 +0300

    hide the voting component before authentication

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -13,7 +13,7 @@ export class SessionListComponent implements OnChanges{
   @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
-  constructor(private auth: AuthService, private voterService: VoterService) {}
+  constructor(public auth: AuthService, private voterService: VoterService) {}
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
