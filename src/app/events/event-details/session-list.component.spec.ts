3cf0b27eac18fd1d85c1f3cbfc85c06e388cd326
collinewait <collinewait17@gmail.com>
2019-04-28 19:11:23 +0300

    test ngOnChange on session list component

---
+++ src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,40 @@
+import { ISession } from './../shared/event.modal';
+import { SessionListComponent } from './session-list.component';
+
+describe('SessionListComponent', () => {
+  let component: SessionListComponent;
+  let mockAuthService;
+  let mockVoterService;
+
+  beforeEach(() => {
+    component = new SessionListComponent(mockAuthService, mockVoterService);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = [
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'beginner' },
+        { name: 'session 3', level: 'intermediate' }
+      ] as ISession[];
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+      component.ngOnChanges();
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = [
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' },
+        { name: 'session 2', level: 'intermediate' }
+      ] as ISession[];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+      component.ngOnChanges();
+      expect(component.visibleSessions[2].name).toBe('session 3');
+    });
+  });
+});
