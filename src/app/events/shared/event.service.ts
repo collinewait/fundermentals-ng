1098532c7494375dcbb1b491e871f0497993310e
collinewait <collinewait17@gmail.com>
2019-04-28 15:38:00 +0300

    consume search sessions api endpoint

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -331,24 +331,9 @@ export class EventService {
       .pipe(catchError(this.handleError<IEvent>('saveEvent')));
   }
 
-  searchSessions(searchTerm: string) {
-    const term = searchTerm.toLocaleLowerCase();
-    let results: ISession[] = [];
-
-    EVENTS.forEach(event => {
-      let matchingSessions = event.sessions
-        .filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-      matchingSessions = matchingSessions.map((session: any) => {
-        session.eventId = event.id;
-        return session;
-      });
-      results = results.concat(matchingSessions);
-    });
-    const emitter = new EventEmitter(true); // passing true tells the emiter to deliver its events asynchronously
-    setTimeout(() => {
-      emitter.emit(results);
-    }, 100);
-    return emitter;
+  searchSessions(searchTerm: string): Observable<ISession[]> {
+    return this.http.get<ISession[]>(`api/sessions/search?search=${searchTerm}`)
+      .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
