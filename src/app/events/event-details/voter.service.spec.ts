994bc9bd12a7a6170c91a77498ff9f0d2890f8b9
collinewait <collinewait17@gmail.com>
2019-04-28 18:57:19 +0300

    test mock calls

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -12,12 +12,29 @@ describe('VoterService', () => {
   });
 
   describe('deleteVoter', () => {
+
     it('should remove the voter from the list of voters', () => {
       const session = { id: 6, voters: ['joe', 'john'] };
       mockHttp.delete.and.returnValue(of(false));
       voterService.deleteVoter(3, session as ISession, 'joe');
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('john');
+    });
+
+    it('should call http.delete with the right url', () => {
+      const session = { id: 6, voters: ['joe', 'john'] };
+      mockHttp.delete.and.returnValue(of(false));
+      voterService.deleteVoter(3, session as ISession, 'joe');
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+    });
+  });
+
+  describe('addVoter', () => {
+    it('should call http.delete with the right url', () => {
+      const session = { id: 6, voters: ['john'] };
+      mockHttp.post.and.returnValue(of(false));
+      voterService.addVoter(3, session as ISession, 'joe');
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
     });
   });
 });
